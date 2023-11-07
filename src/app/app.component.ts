49fb08028bb8f017263e66fd66456536be9890d2
Dylan <yourmom12734@gmail.com>
2023-11-06 21:45:12 -0500

    committed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chirpusPizza';
+  menuItems: MenuItem[]=[
+    { name: 'Margherita', category: 'Pizza', price: 9.99 },
+    { name: 'Pepperoni', category: 'Pizza', price: 10.99 },
+    { name: 'Vegetarian', category: 'Pizza', price: 11.99 },
+    { name: 'Garlic Bread', category: 'Appetizer', price: 4.99 },
+    { name: 'Caesar Salad', category: 'Salad', price: 7.99 },
+    { name: 'BBQ Chicken', category: 'Pizza', price: 12.99 },
+    { name: 'Mushroom Risotto', category: 'Pasta', price: 8.99 },
+    { name: 'Cannoli', category: 'Dessert', price: 5.99 }
+  ]
 }
